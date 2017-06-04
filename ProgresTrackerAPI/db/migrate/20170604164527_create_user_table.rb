277470731ad4358a652256485fe864e46ef33c8d
mark azuolas <markazuolas@gmail.com>
2017-06-04 09:52:31 -0700

    create user table

---
+++ ProgresTrackerAPI/db/migrate/20170604164527_create_user_table.rb
@@ -0,0 +1,14 @@
+class CreateUserTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :fname, null: false
+      t.string :lname, null: false
+      t.string :picture_url
+      t.string :email
+      t.string :linkedin_url
+      t.string :github_url
+      t.string :pronouns
+      t.integer :cohort_id
+    end
+  end
+end
