2749af9d07516ad2ae4c2eb5d5913b5003953f41
agkura <agkura.code@gmail.com>
2017-06-06 12:59:59 -0700

    Add Firebase

--- ProgressTrackerMobile/ios/ProgressTrackerMobile/AppDelegate.m
+++ ProgressTrackerMobile/ios/ProgressTrackerMobile/AppDelegate.m
@@ -34,4 +34,15 @@
   return YES;
 }
 
+@import UIKit;
+@import Firebase;
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
+  return YES;
+}
+
 @end
