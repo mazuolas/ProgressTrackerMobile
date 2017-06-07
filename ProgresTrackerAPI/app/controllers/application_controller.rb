87bb58615790e0d45dc83a4594c951a51f75bb6f
Chris Gillespie <christopher.d.gillespie@gmail.com>
2017-06-07 11:27:34 -0700

    allow csrf for api requests

--- ProgresTrackerAPI/app/controllers/application_controller.rb
+++ ProgresTrackerAPI/app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :json_request?
   helper_method :current_user
 
   def current_user
     #placeholder until oAuth is implemented
     User.find_by(id: 1)
   end
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
+  
 end
