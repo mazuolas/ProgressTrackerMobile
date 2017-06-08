6acbf94adda12f39112431182d3bdaa6feef1558
mark azuolas <markazuolas@gmail.com>
2017-06-08 13:04:03 -0700

    add backend route for classmates

--- ProgresTrackerAPI/config/routes.rb
+++ ProgresTrackerAPI/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     get '/user/me', to: 'users#show'
+    get '/classmates', to: 'users#index'
     get '/day/today', to: 'days#show'
     get '/checkins/today', to: 'checkins#show'
     patch '/checkins/today', to: 'checkins#update'
