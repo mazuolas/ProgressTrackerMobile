c3b34a90f0895aa77353bce3c5c6e67e23726cd2
mark azuolas <markazuolas@gmail.com>
2017-06-10 15:28:38 -0700

    create bargraph style page

---
+++ ProgressTrackerMobile/app/styles/bargraph.js
@@ -0,0 +1,35 @@
+export const pageStyle = {
+  justifyContent: 'space-between',
+  height: 170,
+  backgroundColor: 'white',
+  paddingTop: 20,
+  paddingBottom: 20
+}
+
+export const textStyle = {
+  width: 100,
+  backgroundColor: '#C00A0A',
+  color: 'white',
+  fontSize: 15,
+  fontWeight: 'bold',
+  textAlign: 'center',
+  lineHeight: 40,
+  marginLeft: 10,
+  marginRight: 10
+}
+export const numberStyle = {
+  color: 'white',
+  left: 130,
+  top: 20,
+  backgroundColor: 'transparent',
+  zIndex: 9000,
+  position: 'absolute'
+}
+export const barStyle = {
+  left: 20,
+
+}
+export const viewStyle = {
+  flexDirection: 'row',
+  justifyContent: 'flex-start'
+}
