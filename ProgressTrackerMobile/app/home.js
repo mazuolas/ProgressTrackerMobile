245fd71be355783a61e7d45c270119f7a1f00b49
Chris Gillespie <christopher.d.gillespie@gmail.com>
2017-06-10 17:44:30 -0700

    fetches pair data

--- ProgressTrackerMobile/app/home.js
+++ ProgressTrackerMobile/app/home.js
@@ -12,11 +12,55 @@ class Home extends React.Component {
     ),
   };
 
-  render() {
-    return (
-      <Text>Home Page</Text>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      pair: {},
+      pairs: []
+    };
+    this.fetchPairs = this.fetchPairs.bind(this);
   }
+
+  componentDidMount() {
+    this.fetchPairData();
+  }
+
+  fetchPairData() {
+    fetch('https://progresstrackerapi.herokuapp.com/api/pair/today')
+      .then((response) => response.json())
+      .then((pair) => this.setState({pair: pair}))
+      .then(this.fetchPairs);
+  }
+
+  fetchPairs() {
+    fetch('https://progresstrackerapi.herokuapp.com/api/pairs')
+      .then((response) => response.json())
+      .then((pairs) => this.setState({pairs: pairs}));
+  }
+
+  render() {
+    const { pair, pairs } = this.state;
+
+    if (Object.keys(pairs).length >= 1) {
+      return (
+        <View style={{flexDirection: 'column', flex: 1, alignItems: 'center',
+          justifyContent: 'center'}}>
+          <Text>
+            {`Day: ${pair.day}`}
+          </Text>
+          <Text>
+            {`Workstation: ${pair.workstation}`}
+          </Text>
+          <Text>
+            {`Partner: ${pair.partner.fname} ${pair.partner.lname}`}
+          </Text>
+        </View>
+      );
+    } else {
+      return null;
+    }
+  }
+
 }
 
 export default Home;
